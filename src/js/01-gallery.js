a21d3c8e9cd2bd07a7b0fd88dc0dc4e8a34569cc
michalwojcik4 <mwjcik4@gmail.com>
2023-08-15 09:50:47 +0200

    finished first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const items = []; 
+for(const galleryItem of galleryItems){
+    const li = document.createElement('li');
+
+    const a = document.createElement('a');
+    a.classList.add('gallery__item');
+    a.href = galleryItem.original;
+    li.append(a);
+
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.src = galleryItem.preview;
+    img.alt = galleryItem.description;
+    a.append(img);
+
+    items.push(li);
+}
+
+gallery.append(...items);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+new SimpleLightbox('.gallery__item', {
+    nav: false,
+    captionsData: 'alt',
+    captionDelay: 250,
+    showCounter: false,
+    swipeClose: false,
+});
