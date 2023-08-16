433ff5c5ebc6129a048430883ab8c4f95f768d14
michalwojcik4 <mwjcik4@gmail.com>
2023-08-16 23:42:09 +0200

    finished task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,17 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const keyCurrentTime = "videoplayer-current-time";
 
 player.on('timeupdate', throttle(function() {
     player.getCurrentTime().then(function(seconds) {
-        localStorage.setItem("videoplayer-current-time", seconds);
+        localStorage.setItem(keyCurrentTime, seconds);
     }).catch(function(error) {
         console.log(error.message)
     });
 }, 1000));
 
-const currentTime = localStorage.getItem("videoplayer-current-time")
+const currentTime = localStorage.getItem(keyCurrentTime)
 player.setCurrentTime(currentTime).then().catch(function(error) {
     switch (error.message) {
         case 'RangeError':
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const keyDataForm = "feedback-form-state";
+
+savedLocalStorage();
+form.addEventListener('input', throttle(dataForm), 500);
+form.addEventListener('submit', submitForm);
+
+function dataForm(e) {
+    e.preventDefault();
+    const {elements: {email, message}} = e.currentTarget;
+
+    const user = {
+        email: email.value,
+        message: message.value
+    }
+
+    localStorage.setItem(keyDataForm, JSON.stringify(user));
+};
+
+function savedLocalStorage(){
+    try{
+        const savedData = JSON.parse(localStorage.getItem(keyDataForm));
+        const emailForm = document.querySelector('.feedback-form input');
+        const messageForm = document.querySelector('.feedback-form textarea');
+    
+        if(savedData){
+            emailForm.value = savedData.email;
+            messageForm.value = savedData.message;
+        }
+
+    }catch (error) {
+        console.log(error.message);
+    }
+};
+
+function submitForm(e){
+    e.preventDefault();
+    const {elements: {email, message}} = e.currentTarget;
+
+    if(email.value === ''){
+        return alert('Podaj email!');
+    }
+
+    try{
+        const savedData = JSON.parse(localStorage.getItem(keyDataForm));
+        console.log(savedData);
+    }catch{
+        console.log(error.message);
+    }
+
+    email.value = '';
+    message.value = '';
+    localStorage.removeItem(keyDataForm); 
+};
