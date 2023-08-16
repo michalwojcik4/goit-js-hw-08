54d7850119f7f29ecd2a57186e03d689dc50b5c2
michalwojcik4 <mwjcik4@gmail.com>
2023-08-16 22:13:42 +0200

    finished task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+    }).catch(function(error) {
+        console.log(error.message)
+    });
+}, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(currentTime).then().catch(function(error) {
+    switch (error.message) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
